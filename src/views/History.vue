da49d20a2f86d90f3db7c184176b7677bd7d57ac
glebkiselev11 <12bny23gleb@gmail.com>
2019-09-29 13:52:10 +0300

    Added history page

--- src/views/History.vue
+++ src/views/History.vue
@@ -8,36 +8,54 @@
       <canvas></canvas>
     </div>
 
-    <section>
-      <table>
-        <thead>
-        <tr>
-          <th>#</th>
-          <th>Сумма</th>
-          <th>Дата</th>
-          <th>Категория</th>
-          <th>Тип</th>
-          <th>Открыть</th>
-        </tr>
-        </thead>
+    <Loader v-if="loading" />
 
-        <tbody>
-        <tr>
-          <td>1</td>
-          <td>1212</td>
-          <td>12.12.32</td>
-          <td>name</td>
-          <td>
-            <span class="white-text badge red">Расход</span>
-          </td>
-          <td>
-            <button class="btn-small btn">
-              <i class="material-icons">open_in_new</i>
-            </button>
-          </td>
-        </tr>
-        </tbody>
-      </table>
+    <!-- Если в таблице ничего нет, то показываем этот текст -->
+    <p class="center" v-else-if="!records.length">
+      Записей пока нет. 
+      <router-link to="/record" >Добавьте первую</router-link>
+    </p>  
+
+    <!-- Если же есть, то показываем саму таблицу -->
+    <section v-else>
+      <HistoryTable :records="records"/>
     </section>
   </div>
 </template>
+
+<script>
+import HistoryTable from '@/components/HistoryTable'
+export default {
+  name: 'history',
+  data: () => ({
+    loading: true,
+    records: [],
+    categories: []
+  }),
+  components: {
+    HistoryTable, // Компонент таблицы
+  },
+  async mounted() {
+    // Заходя на страницу мы сразу подгружаем нужные данные из api
+    // this.records = await this.$store.dispatch('fetchRecords')
+    const records = await this.$store.dispatch('fetchRecords')
+    this.categories = await this.$store.dispatch('fetchCategories')
+
+    this.records = records.map(record => {
+      return {
+        ...record,
+        categoryName: this.categories.find(c => c.id === record.categoryId).title, // Тут мы получаем имя категории
+        typeClass: record.type === 'income' ? 'green' : 'red', // Ставим цвет колонки
+        typeText: record.type === 'income' ? 'Доход' : 'Расход', // Ставим нужный текст
+
+
+
+      }
+    })
+    
+
+    this.loading = false
+    
+  },
+}
+</script>
